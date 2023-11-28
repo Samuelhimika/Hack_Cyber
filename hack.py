f623101cc6609eadc37109a5a10de4791459823f
Samuelhimika <himikasa45@gmail.com>
2023-11-28 10:56:40 +0300

    initial commit

---
+++ hack.py
@@ -0,0 +1,12 @@
+from string import ascii_letters,digits, punctuation
+
+for i in ascii_letters + digits + punctuation:
+    for j in ascii_letters + digits + punctuation:
+       for k in ascii_letters + digits + punctuation:
+        for h in ascii_letters + digits + punctuation:
+
+
+            print(i,j,k,h)
+
+
+
